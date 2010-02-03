eaee3b8e1a80b77c07228c61103dc1dd51998b4a
Frederik Naujoks <jfn@gmx.de>
2010-02-03 19:57:35 +0000

    Initial commit.

---
+++ gwiki-webapp/src/main/external_resources/gwikistandalone/gwikiweb.cmd
@@ -0,0 +1,6 @@
+@echo off
+echo start browser with http://localhost:8081/index
+echo Users are gwikisu, gwikieditor, gwikiview. All with password gwiki
+
+set JAVAHOME="c:\Program Files\Java\jdk1.6.0_07"
+%JAVAHOME%\bin\java -jar jettygwiki-0.3.3.jar de.micromata.genome.gwiki.jetty.GWikiJettyStarter
