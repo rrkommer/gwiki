fce93f3e771117119567ba872883791bf0b63afb
Roger Rene Kommer <rkommer@artefaktur.com>
2010-03-06 11:49:29 +0000

    User Preferences, fixes for skins

---
+++ gwiki/src/main/resources/static/gwiki/gwiki-userprefs-0.3.js
@@ -0,0 +1,10 @@
+
+function gwikiSaveUserPrev(key, value, persist)
+{
+	 jQuery.ajax({
+     url: gwikiHomeUrl + "/edit/UserPrefAsync",
+     type: 'POST',
+     dataType: "json",
+     data: { key: key, value: value, persist: persist, method_onSave: true }
+ });
+}
