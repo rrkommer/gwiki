b48585494a46dd249bf24a0cc529ad50390e18fc
Roger Rene Kommer <rkommer@artefaktur.com>
2010-03-30 14:18:03 +0000

    gwiki include, i18n

--- gwiki/src/main/java/de/micromata/genome/gwiki/page/impl/wiki/fragment/GWikiCollectMacroFragmentVisitor.java
+++ gwiki/src/main/java/de/micromata/genome/gwiki/page/impl/wiki/fragment/GWikiCollectMacroFragmentVisitor.java
@@ -18,16 +18,42 @@
 
 package de.micromata.genome.gwiki.page.impl.wiki.fragment;
 
-import de.micromata.genome.gwiki.page.impl.wiki.GWikiMacroFragment;
+import java.util.HashMap;
+import java.util.Map;
 
+import de.micromata.genome.gwiki.page.impl.wiki.GWikiMacroFragment;
+import de.micromata.genome.util.matcher.BooleanListRulesFactory;
+import de.micromata.genome.util.matcher.Matcher;
+
+/**
+ * Visitor to find macros with name and args.
+ * 
+ * @author Roger Rene Kommer (r.kommer@micromata.de)
+ * 
+ */
 public class GWikiCollectMacroFragmentVisitor extends GWikiCollectFragmentTypeVisitor
 {
   private String macroName;
+
+  /**
+   * Key name of macro attribute, value matcher expression.
+   */
+  private Map<String, String> attributeMatcher;
+
+  private Map<String, Matcher<String>> attrMatcher = null;
 
   public GWikiCollectMacroFragmentVisitor(String macroName)
   {
     super(GWikiMacroFragment.class);
     this.macroName = macroName;
+  }
+
+  public GWikiCollectMacroFragmentVisitor(String macroName, Map<String, String> attributeMatcher)
+  {
+    super(GWikiMacroFragment.class);
+    this.macroName = macroName;
+    setAttributeMatcher(attributeMatcher);
+
   }
 
   @Override
@@ -40,6 +66,54 @@ public class GWikiCollectMacroFragmentVisitor extends GWikiCollectFragmentTypeVi
     if (mf.getAttrs().getCmd().equals(macroName) == false) {
       return;
     }
+    if (attrMatcher != null) {
+      for (Map.Entry<String, Matcher<String>> me : attrMatcher.entrySet()) {
+        String val = mf.getAttrs().getArgs().getStringValue(me.getKey());
+        if (me.getValue().match(val) == false) {
+          return;
+        }
+      }
+
+    }
     found.add(fragment);
+  }
+
+  public String getMacroName()
+  {
+    return macroName;
+  }
+
+  public void setMacroName(String macroName)
+  {
+    this.macroName = macroName;
+  }
+
+  public Map<String, String> getAttributeMatcher()
+  {
+    return attributeMatcher;
+  }
+
+  public void setAttributeMatcher(Map<String, String> attributeMatcher)
+  {
+    if (attributeMatcher == null) {
+      this.attrMatcher = null;
+    } else {
+      BooleanListRulesFactory<String> fac = new BooleanListRulesFactory<String>();
+      attrMatcher = new HashMap<String, Matcher<String>>(attributeMatcher.size());
+      for (Map.Entry<String, String> me : attributeMatcher.entrySet()) {
+        attrMatcher.put(me.getKey(), fac.createMatcher(me.getValue()));
+      }
+    }
+    this.attributeMatcher = attributeMatcher;
+  }
+
+  public Map<String, Matcher<String>> getAttrMatcher()
+  {
+    return attrMatcher;
+  }
+
+  public void setAttrMatcher(Map<String, Matcher<String>> attrMatcher)
+  {
+    this.attrMatcher = attrMatcher;
   }
 }
---
+++ gwiki/src/main/java/de/micromata/genome/gwiki/page/impl/wiki/macros/GWikiChunkMacro.java
@@ -0,0 +1,94 @@
+////////////////////////////////////////////////////////////////////////////
+// 
+// Copyright (C) 2010 Micromata GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+////////////////////////////////////////////////////////////////////////////
+package de.micromata.genome.gwiki.page.impl.wiki.macros;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+import de.micromata.genome.gwiki.page.GWikiContext;
+import de.micromata.genome.gwiki.page.impl.wiki.GWikiBodyEvalMacro;
+import de.micromata.genome.gwiki.page.impl.wiki.GWikiMacroBean;
+import de.micromata.genome.gwiki.page.impl.wiki.GWikiMacroRenderFlags;
+import de.micromata.genome.gwiki.page.impl.wiki.MacroAttributes;
+
+/**
+ * Defines a chunk of wiki text inside a wiki page.
+ * 
+ * @author Roger Rene Kommer (r.kommer@micromata.de)
+ * 
+ */
+public class GWikiChunkMacro extends GWikiMacroBean implements GWikiBodyEvalMacro
+{
+
+  private static final long serialVersionUID = 1687669732947013961L;
+
+  /**
+   * used as request parameter and attribute.
+   */
+  public static final String REQUESTATTR_GWIKICHUNK = "gwikichunk";
+
+  private String name;
+
+  private boolean noDecoration;
+
+  public GWikiChunkMacro()
+  {
+    setRenderModes(GWikiMacroRenderFlags.TrimTextContent.getFlag());
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.page.impl.wiki.GWikiMacroBean#renderImpl(de.micromata.genome.gwiki.page.GWikiContext,
+   * de.micromata.genome.gwiki.page.impl.wiki.MacroAttributes)
+   */
+  @Override
+  public boolean renderImpl(GWikiContext ctx, MacroAttributes attrs)
+  {
+    if (noDecoration == false) {
+      ctx.append("<fieldset class=\"gwikichunk\"><legend class=\"gwikichunklegen\">").append(StringEscapeUtils.escapeHtml(name)).append(
+          "</legend>");
+    }
+    if (attrs.getChildFragment() != null) {
+      attrs.getChildFragment().render(ctx);
+    }
+    if (noDecoration == false) {
+      ctx.append("</fieldset>");
+    }
+    return true;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public boolean isNoDecoration()
+  {
+    return noDecoration;
+  }
+
+  public void setNoDecoration(boolean noDecoration)
+  {
+    this.noDecoration = noDecoration;
+  }
+}
