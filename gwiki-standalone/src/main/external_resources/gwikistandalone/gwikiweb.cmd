a4b88802404d66c24dca2ef32c4127b194963aca
Roger Rene Kommer <rkommer@artefaktur.com>
2010-08-31 09:25:42 +0000

    Version numbers

--- gwiki-standalone/src/main/external_resources/gwikistandalone/gwikiweb.cmd
+++ gwiki-standalone/src/main/external_resources/gwikistandalone/gwikiweb.cmd
@@ -3,4 +3,4 @@ echo start browser with http://localhost:8081/index
 echo Users are gwikisu, gwikieditor, gwikiview. All with password gwiki
 
 
-java -jar gwiki-standalone-0.3.1-SNAPSHOT.jar de.micromata.genome.gwiki.jetty.GWikiJettyStarter
+java -jar gwiki-standalone-0.4.2-SNAPSHOT.jar de.micromata.genome.gwiki.jetty.GWikiJettyStarter
