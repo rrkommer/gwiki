628c7b0e76e889f0ac2eb537063968848dd64ed6
Roger Rene Kommer <rkommer@artefaktur.com>
2011-01-06 14:26:30 +0000

    GWIKI-26 Lifecycle Plugin 1.0

---
+++ gwiki/src/main/java/de/micromata/genome/gwiki/model/mpt/GWikiCombinedPageCache.java
@@ -0,0 +1,297 @@
+////////////////////////////////////////////////////////////////////////////
+// 
+// Copyright (C) 2010 Micromata GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+////////////////////////////////////////////////////////////////////////////
+package de.micromata.genome.gwiki.model.mpt;
+
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.apache.commons.lang.StringUtils;
+
+import de.micromata.genome.gwiki.model.GWikiArtefakt;
+import de.micromata.genome.gwiki.model.GWikiElement;
+import de.micromata.genome.gwiki.model.GWikiElementInfo;
+import de.micromata.genome.gwiki.model.GWikiPageCache;
+import de.micromata.genome.gwiki.model.GWikiPropKeys;
+import de.micromata.genome.gwiki.model.GWikiWeb;
+
+/**
+ * Combined page cache
+ * 
+ * TODO deleted pages will not be recognised.
+ * 
+ * @author Roger Rene Kommer (r.kommer@micromata.de)
+ * 
+ */
+public class GWikiCombinedPageCache implements GWikiPageCache
+{
+  /**
+   * This is the primary cache, which may hold additanally/modified pages.
+   */
+  private GWikiPageCache primaryCache;
+
+  /**
+   * secondary/orignal/root cache.
+   */
+  private GWikiPageCache secondaryCache;
+
+  public GWikiCombinedPageCache(GWikiPageCache primaryCache, GWikiPageCache secondaryCache)
+  {
+    this.primaryCache = primaryCache;
+    this.secondaryCache = secondaryCache;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#clearCachedPage(java.lang.String)
+   */
+  public void clearCachedPage(String pageId)
+  {
+    primaryCache.clearCachedPage(pageId);
+    secondaryCache.clearCachedPage(pageId);
+
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#clearCachedPages()
+   */
+  public void clearCachedPages()
+  {
+    primaryCache.clearCachedPages();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#clearCompiledFragments(java.lang.Class)
+   */
+  public void clearCompiledFragments(Class< ? extends GWikiArtefakt< ? extends Serializable>> toClear)
+  {
+    primaryCache.clearCompiledFragments(toClear);
+    secondaryCache.clearCompiledFragments(toClear);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#getPage(java.lang.String)
+   */
+  public GWikiElement getPage(String pageId)
+  {
+    if (primaryCache.hasPageInfo(pageId) == true) {
+      return primaryCache.getPage(pageId);
+    }
+    return secondaryCache.getPage(pageId);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#getPageInfo(java.lang.String)
+   */
+  public GWikiElementInfo getPageInfo(String pageId)
+  {
+
+    GWikiElementInfo ei = primaryCache.getPageInfo(pageId);
+    if (ei != null) {
+      return ei;
+    }
+    return secondaryCache.getPageInfo(pageId);
+  }
+
+  public int getElementInfoCount()
+  {
+    return primaryCache.getElementInfoCount() + secondaryCache.getElementInfoCount();
+  }
+
+  class CombinedElementInfoIterator implements Iterator<GWikiElementInfo>
+  {
+    boolean inSecondary = false;
+
+    Iterator<GWikiElementInfo> primary;
+
+    Iterator<GWikiElementInfo> secondary;
+
+    GWikiElementInfo secNext = null;
+
+    public CombinedElementInfoIterator()
+    {
+      primary = primaryCache.getPageInfos().iterator();
+      secondary = secondaryCache.getPageInfos().iterator();
+    }
+
+    private void seekNextSec()
+    {
+      if (secNext != null) {
+        return;
+      }
+      if (secondary.hasNext() == false) {
+        return;
+      }
+      secNext = secondary.next();
+      if (primaryCache.hasCachedPage(secNext.getId()) == false) {
+        return;
+      }
+      secNext = null;
+      seekNextSec();
+    }
+
+    public boolean hasNext()
+    {
+      if (inSecondary == false) {
+        if (primary.hasNext() == true) {
+          return true;
+        }
+        inSecondary = true;
+        return hasNext();
+      }
+      if (secondary.hasNext() == false) {
+        return false;
+      }
+      seekNextSec();
+      return secNext != null;
+    }
+
+    public GWikiElementInfo next()
+    {
+
+      if (inSecondary == false) {
+        return primary.next();
+      }
+      seekNextSec();
+      if (secNext == null) {
+        throw new NoSuchElementException();
+      }
+      GWikiElementInfo ret = secNext;
+      secNext = null;
+      return ret;
+    }
+
+    public void remove()
+    {
+      throw new UnsupportedOperationException("Removing page info not supported");
+
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#getPageInfos()
+   */
+  public Iterable<GWikiElementInfo> getPageInfos()
+  {
+    return new Iterable<GWikiElementInfo>() {
+
+      public Iterator<GWikiElementInfo> iterator()
+      {
+        return new CombinedElementInfoIterator();
+      }
+    };
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#hasCachedPage(java.lang.String)
+   */
+  public boolean hasCachedPage(String pageId)
+  {
+    return primaryCache.hasCachedPage(pageId) == true || secondaryCache.hasCachedPage(pageId);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#hasPageInfo(java.lang.String)
+   */
+  public boolean hasPageInfo(String pageId)
+  {
+    return primaryCache.hasPageInfo(pageId) == true || secondaryCache.hasPageInfo(pageId) == true;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#initPageCache(de.micromata.genome.gwiki.model.GWikiWeb)
+   */
+  public void initPageCache(GWikiWeb wikiWeb)
+  {
+    primaryCache.initPageCache(wikiWeb);
+
+  }
+
+  protected GWikiPageCache getCacheToWrite(GWikiElementInfo ei)
+  {
+    if (primaryCache.hasPageInfo(ei.getId()) == true) {
+      return primaryCache;
+    }
+    String tid = ei.getProps().getStringValue(GWikiPropKeys.TENANT_ID);
+    if (StringUtils.isNotEmpty(tid) == true) {
+      if (StringUtils.equals(GWikiWeb.get().getTenantId(), tid) == true) {
+        return primaryCache;
+      }
+    }
+    return secondaryCache;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#putCachedPage(java.lang.String, de.micromata.genome.gwiki.model.GWikiElement)
+   */
+  public void putCachedPage(String pageId, GWikiElement el)
+  {
+    getCacheToWrite(el.getElementInfo()).putCachedPage(pageId, el);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#putPageInfo(de.micromata.genome.gwiki.model.GWikiElementInfo)
+   */
+  public void putPageInfo(GWikiElementInfo ei)
+  {
+    getCacheToWrite(ei).putPageInfo(ei);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#removePageInfo(java.lang.String)
+   */
+  public void removePageInfo(String pageId)
+  {
+    primaryCache.removePageInfo(pageId);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.micromata.genome.gwiki.model.GWikiPageCache#setPageInfoMap(java.util.Map)
+   */
+  public void setPageInfoMap(Map<String, GWikiElementInfo> pageInfos)
+  {
+    primaryCache.setPageInfoMap(pageInfos);
+  }
+
+}
---
+++ gwiki/src/main/java/de/micromata/genome/gwiki/model/mpt/GWikiMultipleWikiSelector.java
@@ -0,0 +1,209 @@
+////////////////////////////////////////////////////////////////////////////
+// 
+// Copyright (C) 2010 Micromata GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+////////////////////////////////////////////////////////////////////////////
+package de.micromata.genome.gwiki.model.mpt;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.micromata.genome.gdbfs.FileSystem;
+import de.micromata.genome.gdbfs.FsObject;
+import de.micromata.genome.gdbfs.ReadWriteCombinedFileSystem;
+import de.micromata.genome.gwiki.model.GWikiElement;
+import de.micromata.genome.gwiki.model.GWikiElementInfo;
+import de.micromata.genome.gwiki.model.GWikiPageCacheTimedImpl;
+import de.micromata.genome.gwiki.model.GWikiPropKeys;
+import de.micromata.genome.gwiki.model.GWikiStandardWikiSelector;
+import de.micromata.genome.gwiki.model.GWikiStorage;
+import de.micromata.genome.gwiki.model.GWikiWeb;
+import de.micromata.genome.gwiki.model.config.GWikiDAOContext;
+import de.micromata.genome.gwiki.model.filter.GWikiFilterChain;
+import de.micromata.genome.gwiki.model.filter.GWikiLoadElementInfosFilter;
+import de.micromata.genome.gwiki.model.filter.GWikiLoadElementInfosFilterEvent;
+import de.micromata.genome.gwiki.page.GWikiContext;
+import de.micromata.genome.gwiki.page.GWikiStandaloneContext;
+import de.micromata.genome.gwiki.spi.storage.GWikiFileStorage;
+import de.micromata.genome.gwiki.web.GWikiServlet;
+import de.micromata.genome.util.runtime.CallableX;
+
+/**
+ * @author Roger Rene Kommer (r.kommer@micromata.de)
+ * 
+ */
+public class GWikiMultipleWikiSelector extends GWikiStandardWikiSelector
+{
+
+  private GWikiMptIdSelector mptIdSelector = new GWikiReqSessionMptIdSelector();
+
+  private Map<String, GWikiWeb> customWikis = Collections.synchronizedMap(new HashMap<String, GWikiWeb>());
+
+  private ThreadLocal<GWikiWeb> THREAD_WIKI = new ThreadLocal<GWikiWeb>() {
+
+    @Override
+    protected GWikiWeb initialValue()
+    {
+      return wiki;
+    }
+
+  };
+
+  public GWikiWeb getWikiWeb(GWikiServlet servlet)
+  {
+    final GWikiWeb wiki = THREAD_WIKI.get();
+    if (wiki != null) {
+      return wiki;
+    }
+    return super.getWikiWeb(servlet);
+  }
+
+  protected String getTenantId(GWikiServlet servlet, HttpServletRequest req)
+  {
+    return mptIdSelector.getTenantId(servlet, req);
+  }
+
+  protected void loadDerivedWiki(final GWikiWeb wikiWeb)
+  {
+    wikiWeb.runInPluginContext(new CallableX<Void, RuntimeException>() {
+
+      public Void call() throws RuntimeException
+      {
+
+        // modCheckTimoutMs = config.getCheckFileSystemForModTimeout();
+        Map<String, GWikiElementInfo> npageInfos = new HashMap<String, GWikiElementInfo>();
+        npageInfos = wikiWeb.getFilter().loadPageInfos(GWikiContext.getCurrent(), npageInfos, new GWikiLoadElementInfosFilter() {
+          public Void filter(GWikiFilterChain<Void, GWikiLoadElementInfosFilterEvent, GWikiLoadElementInfosFilter> chain,
+              GWikiLoadElementInfosFilterEvent event)
+          {
+            wikiWeb.getStorage().loadPageInfos(event.getPageInfos());
+            // lastModCounter = getStorage().getModificationCounter();
+            wikiWeb.getDaoContext().getPageCache().setPageInfoMap(event.getPageInfos());
+            return null;
+          }
+        });
+        return null;
+      }
+    });
+  }
+
+  protected GWikiWeb createDerivedWiki(GWikiServlet servlet, final String cid)
+  {
+    GWikiWeb rootWiki = getWikiWeb(servlet);
+    GWikiDAOContext mptDaoContext = new GWikiDAOContext(rootWiki.getDaoContext());
+
+    final FileSystem mptFsSp = mptIdSelector.getTenantFileSystem(rootWiki, cid);
+    ReadWriteCombinedFileSystem cmp = new ReadWriteCombinedFileSystem(mptFsSp, rootWiki.getDaoContext().getStorage().getFileSystem());
+
+    GWikiStorage storage = new GWikiFileStorage(mptFsSp) {
+
+      protected GWikiElementInfo afterPageInfoLoad(FsObject fo, GWikiElementInfo el, final Map<String, GWikiElementInfo> ret)
+      {
+        if (ret.containsKey(el.getId()) == true) {
+          return null;
+        }
+        if (fo.getFileSystem() == mptFsSp) {
+          el.getProps().setStringValue(GWikiPropKeys.TENANT_ID, cid);
+        }
+        return el;
+      }
+
+      public GWikiElement storeElement(final GWikiContext wikiContext, final GWikiElement elm, final boolean keepModifiedAt)
+      {
+        GWikiElement el = super.storeElement(wikiContext, elm, keepModifiedAt);
+        el.getElementInfo().getProps().setStringValue(GWikiPropKeys.TENANT_ID, cid);
+        return el;
+      }
+    };
+    mptDaoContext.setStorage(storage);
+    GWikiPageCacheTimedImpl pageCache = new GWikiPageCacheTimedImpl();
+
+    GWikiCombinedPageCache combPageCache = new GWikiCombinedPageCache(pageCache, rootWiki.getDaoContext().getPageCache());
+    // challenge:
+    // Cache should be combined, where cached objects should
+    // detect if owned by root gwiki or derived.
+
+    // both for pageinfo and for page content
+
+    mptDaoContext.setPageCache(combPageCache);
+    GWikiWeb mptWiki = new GWikiWeb(rootWiki);
+    mptWiki.setTenantId(cid);
+    storage.setWikiWeb(mptWiki);
+    mptWiki.setDaoContext(mptDaoContext);
+    pageCache.initPageCache(mptWiki);
+
+    GWikiStandaloneContext wikiContext = new GWikiStandaloneContext(mptWiki, servlet, servlet.getContextPath(), servlet.getServletPath());
+    try {
+      GWikiContext.setCurrent(wikiContext);
+      loadDerivedWiki(mptWiki);
+    } finally {
+      GWikiContext.setCurrent(null);
+    }
+    storage.setFileSystem(cmp);
+    return mptWiki;
+  }
+
+  @Override
+  public void initWiki(GWikiServlet servlet, HttpServletRequest req, HttpServletResponse resp)
+  {
+    super.initWiki(servlet, req, resp);
+
+    final String cid = getTenantId(servlet, req);
+    if (cid == null) {
+      THREAD_WIKI.set(wiki);
+      return;
+    }
+    GWikiWeb wiki = customWikis.get(cid);
+    if (wiki != null) {
+      THREAD_WIKI.set(wiki);
+      return;
+    }
+    wiki = createDerivedWiki(servlet, cid);
+    customWikis.put(cid, wiki);
+    THREAD_WIKI.set(wiki);
+    // TODO plc deinitwiki per request cyclus
+  }
+
+  public void clearTenant(GWikiContext wikiContext, String tenantId)
+  {
+    customWikis.remove(tenantId);
+  }
+
+  public void deinitWiki(GWikiServlet servlet, HttpServletRequest req, HttpServletResponse resp)
+  {
+    THREAD_WIKI.remove();
+  }
+
+  public GWikiMptIdSelector getMptIdSelector()
+  {
+    return mptIdSelector;
+  }
+
+  public void setMptIdSelector(GWikiMptIdSelector mptIdSelector)
+  {
+    this.mptIdSelector = mptIdSelector;
+  }
+
+  @Override
+  public void enterTenant(GWikiContext wikiContext, String tenantId)
+  {
+    mptIdSelector.setTenant(wikiContext, tenantId);
+    initWiki(GWikiServlet.INSTANCE, wikiContext.getRequest(), wikiContext.getResponse());
+  }
+}
