d3d4f26d672259d68416f8aedd745cb5f6fc5165
Stefan Stuetzer <s.stuetzer@micromata.de>
2011-01-20 13:30:58 +0000

    Maven Plugin Pagelifecycle

---
+++ plugins/gwiki-pagelifecycle/src/main/java/de/micromata/genome/gwiki/pagelifecycle_1_0/action/CreateBranchActionBean.java
@@ -0,0 +1,231 @@
+////////////////////////////////////////////////////////////////////////////
+// 
+// Copyright (C) 2010 Micromata GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+////////////////////////////////////////////////////////////////////////////
+package de.micromata.genome.gwiki.pagelifecycle_1_0.action;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang.StringUtils;
+
+import de.micromata.genome.gwiki.model.GWikiArtefakt;
+import de.micromata.genome.gwiki.model.GWikiElement;
+import de.micromata.genome.gwiki.model.GWikiProps;
+import de.micromata.genome.gwiki.model.GWikiPropsArtefakt;
+import de.micromata.genome.gwiki.model.GWikiWebUtils;
+import de.micromata.genome.gwiki.model.GWikiWikiSelector;
+import de.micromata.genome.gwiki.model.mpt.GWikiMultipleWikiSelector;
+import de.micromata.genome.gwiki.page.impl.actionbean.ActionBeanBase;
+import de.micromata.genome.gwiki.pagelifecycle_1_0.model.BranchState;
+
+/**
+ * Actionbean for creating new branches (content-releases)
+ * 
+ * @author Stefan Stuetzer (s.stuetzer@micromata.com) 
+ */
+public class CreateBranchActionBean extends ActionBeanBase
+{
+  private static final String INFO_TEMPLATE_ID      = "admin/templates/GWikiBranchInfoElementTemplate";
+  private static final String FILESTATS_TEMPLATE_ID = "admin/templates/GWikiBranchFileStatsTemplate";
+
+  private String branchId;
+
+  private String description;
+  
+  private String releaseStartDate;
+  
+  private String releaseEndDate;
+  
+  public Object onCreateBranch() {
+    if (isBranchIdValid() == false) {
+      return null;
+    }
+    if (isReleaseDateValid() == false) {
+      return null;
+    }
+    
+    GWikiWikiSelector wikiSelector = wikiContext.getWikiWeb().getDaoContext().getWikiSelector();
+    if (wikiSelector instanceof GWikiMultipleWikiSelector == false) {
+      wikiContext.addSimpleValidationError("No multiple branches supported.");
+      return null;
+    }
+    
+    // create or enter new branch
+    wikiSelector.enterTenant(wikiContext, this.branchId);
+    
+    createBranchInfoElement();
+    createBranchFileStats();
+    
+    // leave branch
+    wikiSelector.leaveTenant(wikiContext);
+    
+    return null;
+  }
+
+  private void createBranchFileStats()
+  {
+    GWikiElement el = GWikiWebUtils.createNewElement(wikiContext, "admin/branch/BranchFileStats", FILESTATS_TEMPLATE_ID, "Branch File Stats");
+    wikiContext.getWikiWeb().saveElement(wikiContext, el, false);
+  }
+
+  /**
+   * Creates initial the branch info element dolding branch meta data
+   */
+  private void createBranchInfoElement()
+  {
+    GWikiElement el = GWikiWebUtils.createNewElement(wikiContext, "admin/branch/BranchInfoElement", INFO_TEMPLATE_ID, "BranchInfo");
+    GWikiArtefakt< ? > artefakt = el.getMainPart();
+
+    GWikiPropsArtefakt art = (GWikiPropsArtefakt) artefakt;
+    GWikiProps props = art.getCompiledObject();
+    props.setStringValue("BRANCH_ID", this.branchId);
+    props.setStringValue("DESCRIPTION", this.description);
+    props.setStringValue("BRANCH_STATE", BranchState.OFFLINE.name());
+    props.setStringValue("RELEASE_DATE", getDateString(this.releaseStartDate));
+    props.setStringValue("RELEASE_END_DATE", getDateString(this.releaseEndDate));
+    
+    wikiContext.getWikiWeb().saveElement(wikiContext, el, false);
+  }
+
+  private boolean isBranchIdValid()
+  {
+    if (StringUtils.isBlank(this.branchId) == true) {
+      wikiContext.addSimpleValidationError("branch id not valid. It must have at least one character");
+      return false;
+    }
+    if (StringUtils.containsAny(this.branchId, new char[]{'/',',','*','#','"','\'',' '})) {
+      wikiContext.addSimpleValidationError("Branch-Id not valid. It contains invalid chracters.");
+      return false;
+    }
+    if (this.branchId.length() > 50) {
+      wikiContext.addSimpleValidationError("Branch-Id not valid. There are too many characters.");
+      return false;
+    }
+    return true;
+  }
+  
+  private boolean isReleaseDateValid()
+  {
+    // no dates given -> true
+    if (StringUtils.isBlank(this.releaseStartDate) == true && StringUtils.isBlank(this.releaseEndDate) == true) {
+      return true;
+    }
+    
+    try {
+      // try to parse date strings
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+      if (StringUtils.isNotBlank(this.releaseStartDate) == true) {
+        sdf.parse(this.releaseStartDate);
+      }
+      if (StringUtils.isNotBlank(this.releaseEndDate) == true) {
+        sdf.parse(this.releaseEndDate);
+      }
+      return true;
+    } catch (ParseException ex) {
+      wikiContext.addSimpleValidationError("Release date(s) not valid");
+
+      return false;
+    }
+  }
+  
+  /**
+   * @param releaseStartDate2
+   * @return
+   */
+  private String getDateString(String date)
+  {
+    if (StringUtils.isBlank(date) == true) {
+      return StringUtils.EMPTY;
+    }
+    
+    try {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    Date parsedDate = sdf.parse(this.releaseStartDate);
+    return GWikiProps.formatTimeStamp(parsedDate);
+    } catch (ParseException ex) {
+      ex.printStackTrace();
+    }
+    return null;
+  }
+
+  
+  /**
+   * @param branchId the branchId to set
+   */
+  public void setBranchId(String branchId)
+  {
+    this.branchId = branchId;
+  }
+
+  /**
+   * @return the branchId
+   */
+  public String getBranchId()
+  {
+    return branchId;
+  }
+
+  /**
+   * @param description the description to set
+   */
+  public void setDescription(String description)
+  {
+    this.description = description;
+  }
+
+  /**
+   * @return the description
+   */
+  public String getDescription()
+  {
+    return description;
+  }
+
+  /**
+   * @param releaseStartDate the releaseStartDate to set
+   */
+  public void setReleaseStartDate(String releaseStartDate)
+  {
+    this.releaseStartDate = releaseStartDate;
+  }
+
+  /**
+   * @return the releaseStartDate
+   */
+  public String getReleaseStartDate()
+  {
+    return releaseStartDate;
+  }
+
+  /**
+   * @param releaseEndDate the releaseEndDate to set
+   */
+  public void setReleaseEndDate(String releaseEndDate)
+  {
+    this.releaseEndDate = releaseEndDate;
+  }
+
+  /**
+   * @return the releaseEndDate
+   */
+  public String getReleaseEndDate()
+  {
+    return releaseEndDate;
+  } 
+
+}
---
+++ plugins/gwiki-pagelifecycle/src/main/java/de/micromata/genome/gwiki/pagelifecycle_1_0/debug/PlcTenantsActionBean.java
@@ -0,0 +1,183 @@
+////////////////////////////////////////////////////////////////////////////
+// 
+// Copyright (C) 2010 Micromata GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+////////////////////////////////////////////////////////////////////////////
+package de.micromata.genome.gwiki.pagelifecycle_1_0.debug;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
+import de.micromata.genome.gwiki.model.GWikiElementInfo;
+import de.micromata.genome.gwiki.model.GWikiPropKeys;
+import de.micromata.genome.gwiki.model.GWikiWeb;
+import de.micromata.genome.gwiki.model.GWikiWikiSelector;
+import de.micromata.genome.gwiki.model.mpt.GWikiMptIdSelector;
+import de.micromata.genome.gwiki.model.mpt.GWikiMultipleWikiSelector;
+import de.micromata.genome.gwiki.page.impl.actionbean.ActionBeanBase;
+import de.micromata.genome.gwiki.scheduler_1_0.api.GWikiScheduler;
+import de.micromata.genome.gwiki.web.GWikiServlet;
+import de.micromata.genome.util.matcher.MatcherBase;
+
+/**
+ * @author Roger Rene Kommer (r.kommer@micromata.de)
+ * 
+ */
+public class PlcTenantsActionBean extends ActionBeanBase
+{
+  private String currentTenant;
+
+  private List<String> availableTenants;
+
+  private String selectedTenant;
+
+  private List<String> tenantPageIds;
+
+  public void init()
+  {
+    GWikiMptIdSelector idsel = getTenantSelector();
+    if (idsel == null) {
+      currentTenant = null;
+      return;
+    }
+    currentTenant = StringUtils.defaultString(idsel.getTenantId(GWikiServlet.INSTANCE, wikiContext.getRequest()));
+    if (StringUtils.isEmpty(currentTenant) == true) {
+      currentTenant = null;
+    }
+  }
+
+  public Object onInit()
+  {
+    init();
+    return null;
+  }
+
+  protected GWikiMptIdSelector getTenantSelector()
+  {
+    GWikiWikiSelector ws = wikiContext.getWikiWeb().getDaoContext().getWikiSelector();
+    if ((ws instanceof GWikiMultipleWikiSelector) == false) {
+      return null;
+    }
+    GWikiMultipleWikiSelector ms = (GWikiMultipleWikiSelector) ws;
+    return ms.getMptIdSelector();
+  }
+
+  public Object onListTenants()
+  {
+    init();
+    GWikiMptIdSelector sel = getTenantSelector();
+    if (sel == null) {
+      wikiContext.addSimpleValidationError("No tenant available");
+      return null;
+    }
+    availableTenants = sel.getTenants(GWikiWeb.getRootWiki());
+    return null;
+  }
+
+  public Object onEnterTenant()
+  {
+    init();
+    if (StringUtils.isBlank(selectedTenant) == true) {
+      selectedTenant = null;
+    }
+    wikiContext.getWikiWeb().getDaoContext().getWikiSelector().enterTenant(wikiContext, selectedTenant);
+    init();
+    return null;
+  }
+
+  public Object onClearTenantCache()
+  {
+    init();
+    if (StringUtils.isBlank(selectedTenant) == true) {
+      return null;
+    }
+    wikiContext.getWikiWeb().getDaoContext().getWikiSelector().clearTenant(wikiContext, selectedTenant);
+    return null;
+  }
+
+  public Object onShowTenantPageIds()
+  {
+    init();
+    if (StringUtils.isEmpty(currentTenant) == true) {
+      wikiContext.addSimpleValidationError("No tenant selected");
+      return null;
+    }
+    List<String> pids = new ArrayList<String>();
+    for (GWikiElementInfo ei : wikiContext.getElementFinder().getPageInfos(new MatcherBase<GWikiElementInfo>() {
+      public boolean match(GWikiElementInfo ei)
+      {
+        String tid = ei.getProps().getStringValue(GWikiPropKeys.TENANT_ID);
+        return StringUtils.equals(currentTenant, tid) == true;
+      }
+    })) {
+      pids.add(ei.getId());
+    }
+    tenantPageIds = pids;
+    return null;
+  }
+
+  public Object onSubmitTestJob()
+  {
+    Map<String, String> sm = new HashMap<String, String>();
+    sm.put("A1", "V1");
+    sm.put("A2", "V2");
+    GWikiScheduler.submitJob(null, TestPubJob.class.getName(), "+10000", sm);
+    return null;
+  }
+
+  public String getCurrentTenant()
+  {
+    return currentTenant;
+  }
+
+  public void setCurrentTenant(String currentTenant)
+  {
+    this.currentTenant = currentTenant;
+  }
+
+  public List<String> getAvailableTenants()
+  {
+    return availableTenants;
+  }
+
+  public void setAvailableTenants(List<String> availableTenants)
+  {
+    this.availableTenants = availableTenants;
+  }
+
+  public String getSelectedTenant()
+  {
+    return selectedTenant;
+  }
+
+  public void setSelectedTenant(String selectedTenant)
+  {
+    this.selectedTenant = selectedTenant;
+  }
+
+  public List<String> getTenantPageIds()
+  {
+    return tenantPageIds;
+  }
+
+  public void setTenantPageIds(List<String> tenantPageIds)
+  {
+    this.tenantPageIds = tenantPageIds;
+  }
+}
