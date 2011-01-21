7cd284f98b663d311d25cfd352f75235018ad219
Ingo Joseph <i.joseph@micromata.de>
2011-01-21 10:15:26 +0000

    Atom-Feed

--- gwiki/src/plugins/rssfeed/src/main/java/de/micromata/genome/gwiki/plugin/rssfeed_1_0/filter/RssFeedFilter.java
+++ gwiki/src/plugins/rssfeed/src/main/java/de/micromata/genome/gwiki/plugin/rssfeed_1_0/filter/RssFeedFilter.java
@@ -16,6 +16,14 @@
 ////////////////////////////////////////////////////////////////////////////
 package de.micromata.genome.gwiki.plugin.rssfeed_1_0.filter;
 
+import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.Atom.content;
+import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.Atom.entry;
+import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.Atom.feed;
+import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.Atom.link;
+import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.Atom.name;
+import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.Atom.subtitle;
+import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.Atom.summary;
+import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.Atom.updated;
 import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.RSS.author;
 import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.RSS.channel;
 import static de.micromata.genome.gwiki.plugin.rssfeed_1_0.RSS.copyright;
@@ -31,6 +39,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import org.apache.commons.lang.StringUtils;
+
 import de.micromata.genome.gwiki.model.GWikiArtefakt;
 import de.micromata.genome.gwiki.model.GWikiElementInfo;
 import de.micromata.genome.gwiki.model.filter.GWikiFilterChain;
@@ -42,7 +52,7 @@ import de.micromata.genome.gwiki.page.RenderModes;
 import de.micromata.genome.gwiki.page.impl.GWikiWikiPageArtefakt;
 import de.micromata.genome.util.xml.xmlbuilder.Xml;
 import de.micromata.genome.util.xml.xmlbuilder.XmlElement;
- 
+
 /**
  * @author Ingo Joseph
  */
@@ -63,8 +73,8 @@ public class RssFeedFilter implements GWikiServeElementFilter
     if (feed == null) {
       return chain.nextFilter(event);
     }
-    // vergleich auf "rss"
-    if ("rss".equals(feed) == false) {
+
+    if ("rss".equals(feed) == false && "atom".equals(feed) == false) {
       return chain.nextFilter(event);
     }
 
@@ -81,31 +91,66 @@ public class RssFeedFilter implements GWikiServeElementFilter
 
       String title = elementInfo.getTitle();
       String wikiPageHtmlContent = standaloneContext.getOutString();
-      String datum = elementInfo.getCreatedAt().toString();
       String autor = elementInfo.getCreatedBy();
 
       DateFormat dfmt = new SimpleDateFormat("EE, d MMM yyyy hh:mm:ss +0100", Locale.ENGLISH);
-      datum = dfmt.format(elementInfo.getCreatedAt());
-
+      String createDate = dfmt.format(elementInfo.getCreatedAt());
+      String currentDate = dfmt.format(new java.util.Date());
       wikiContext.getResponse().setHeader("Content-Type", "application/xml");
 
-      XmlElement RSS = rss("2.0").nest(channel((title(Xml.text("GWiki RSS-Feeds"))),//
-          (link(Xml.text("http://localhost:8081/"))),//
-          (description(Xml.text("Hier siehst du Feeds von GWiki!"))),//
-          (copyright(Xml.text("ingo"))),//
-          (language(Xml.text("de-de"))),//
-          (pubData(Xml.text(dfmt.format(new java.util.Date())))),//
-          (item((title(Xml.text(title))),//
-              (link(Xml.text(wikiContext.getRequest().getRequestURL().toString()))),//
-              (pubData(Xml.text(datum))),//
-              (description(Xml.text(wikiPageHtmlContent))),//
-              (author(Xml.text(autor)))//
-          ))//
-          ));
+      if ("rss".equals(feed)) {
+        renderRSS(wikiContext, title, wikiPageHtmlContent, createDate, currentDate, autor);
+      }
 
-      wikiContext.append(RSS.toString());
-      wikiContext.flush();
+      if ("atom".equals(feed)) {
+        renderAtom(wikiContext, title, wikiPageHtmlContent, createDate, currentDate, autor);
+      }
     }
     return null;
+  }
+
+
+  private void renderAtom(GWikiContext wikiContext, String title, String wikiPageHtmlContent, String createDate, String currentDate, String autor)
+  {
+    XmlElement atom = feed("http://www.w3.org/2005/Atom").nest(author((name(Xml.text("Ingo Joseph")))),//
+        (title(Xml.text("GWiki Atom-Feeds"))),//
+        (link("http://localhost:8081/")),//
+        (subtitle(Xml.text("Hier siehst du Feeds von GWiki!"))),//
+        (updated(Xml.text(currentDate))),//
+        (entry((title(Xml.text(title))),//
+            (link(wikiContext.getRequest().getRequestURL().toString())),//
+            (updated(Xml.text(createDate))),//
+            (summary("html").nest(Xml.text(StringUtils.left(wikiPageHtmlContent, 100)+"[...]"))),//
+            (content("html").nest(Xml.text(wikiPageHtmlContent))),//
+            author((name(Xml.text(autor))))//
+        ))//
+        );
+      wikiContext.append(atom.toString());
+      wikiContext.flush();
+      
+      
+  }
+
+
+  private void renderRSS(GWikiContext wikiContext, String title, String wikiPageHtmlContent, String createDate, String currentDate,
+      String autor)
+  {
+    XmlElement RSS = rss("2.0").nest(channel((title(Xml.text("GWiki RSS-Feeds"))),//
+        (link(Xml.text("http://localhost:8081/"))),//
+        (description(Xml.text("Hier siehst du Feeds von GWiki!"))),//
+        (copyright(Xml.text("ingo"))),//
+        (language(Xml.text("de-de"))),//
+        (pubData(Xml.text(currentDate)))),//
+        (item((title(Xml.text(title))),//
+            (link(Xml.text(wikiContext.getRequest().getRequestURL().toString()))),//
+            (pubData(Xml.text(createDate))),//
+            (description(Xml.text(wikiPageHtmlContent))),//
+            (author(Xml.text(autor)))//
+        ))//
+        );
+
+    wikiContext.append(RSS.toString());
+    wikiContext.flush();
+
   }
 }
