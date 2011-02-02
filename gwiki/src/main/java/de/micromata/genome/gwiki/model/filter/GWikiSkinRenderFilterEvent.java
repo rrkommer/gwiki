1d64ac3c7b7fa056d882ed95f33a08a9f9185508
Roger Rene Kommer <rkommer@artefaktur.com>
2011-02-02 22:46:07 +0000

    https://labs.micromata.de/jira/browse/GWIKI-59

---
+++ gwiki/src/main/java/de/micromata/genome/gwiki/model/filter/GWikiSkinRenderFilterEvent.java
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////
+// 
+// Copyright (C) 2010 Micromata GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+////////////////////////////////////////////////////////////////////////////
+package de.micromata.genome.gwiki.model.filter;
+
+import de.micromata.genome.gwiki.model.GWikiElement;
+import de.micromata.genome.gwiki.page.GWikiContext;
+
+/**
+ * Filter will be called while rendering gui to have the posibility to render additionally markup.
+ * 
+ * @author Roger Rene Kommer (r.kommer@micromata.de)
+ * 
+ */
+public class GWikiSkinRenderFilterEvent extends GWikiElementFilterEvent
+{
+  /**
+   * Gui placements to include:
+   * 
+   * @author Roger Rene Kommer (r.kommer@micromata.de)
+   * 
+   */
+  public static enum GuiElementType
+  {
+    BeginBody, //
+    EndBody, //
+    HeadMenu, //
+    PageMenu, //
+    ;
+  }
+
+  private String guiElementType;
+
+  /**
+   * @param wikiContext
+   * @param element
+   */
+  public GWikiSkinRenderFilterEvent(GWikiContext wikiContext, GWikiElement element, String guiElementType)
+  {
+    super(wikiContext, element);
+    this.guiElementType = guiElementType;
+  }
+
+  public String getGuiElementType()
+  {
+    return guiElementType;
+  }
+
+  public void setGuiElementType(String guiElementType)
+  {
+    this.guiElementType = guiElementType;
+  }
+
+}
