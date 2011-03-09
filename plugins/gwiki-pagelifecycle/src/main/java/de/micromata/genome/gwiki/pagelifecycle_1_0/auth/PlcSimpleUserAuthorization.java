095d0b7b1138431610ad41628e25d6e687d2de89
Christian Claus <c.claus@micromata.de>
2011-03-09 11:38:31 +0000

    added Plc-specific authorization of 'isAllowToEdit'

--- plugins/gwiki-pagelifecycle/src/main/java/de/micromata/genome/gwiki/pagelifecycle_1_0/auth/PlcSimpleUserAuthorization.java
+++ plugins/gwiki-pagelifecycle/src/main/java/de/micromata/genome/gwiki/pagelifecycle_1_0/auth/PlcSimpleUserAuthorization.java
@@ -17,8 +17,20 @@
 ////////////////////////////////////////////////////////////////////////////
 package de.micromata.genome.gwiki.pagelifecycle_1_0.auth;
 
+import org.apache.commons.lang.StringUtils;
+
 import de.micromata.genome.gwiki.auth.GWikiAuthorizationExtWrapper;
 import de.micromata.genome.gwiki.model.GWikiAuthorizationExt;
+import de.micromata.genome.gwiki.model.GWikiAuthorizationRights;
+import de.micromata.genome.gwiki.model.GWikiElementInfo;
+import de.micromata.genome.gwiki.model.GWikiWikiSelector;
+import de.micromata.genome.gwiki.model.mpt.GWikiMultipleWikiSelector;
+import de.micromata.genome.gwiki.page.GWikiContext;
+import de.micromata.genome.gwiki.pagelifecycle_1_0.artefakt.BranchFileStats;
+import de.micromata.genome.gwiki.pagelifecycle_1_0.artefakt.FileStatsDO;
+import de.micromata.genome.gwiki.pagelifecycle_1_0.model.GWikiPlcRights;
+import de.micromata.genome.gwiki.pagelifecycle_1_0.model.PlcUtils;
+import de.micromata.genome.util.runtime.CallableX;
 
 /**
  * @author Christian Claus (c.claus@micromata.de)
@@ -35,4 +47,72 @@ public class PlcSimpleUserAuthorization extends GWikiAuthorizationExtWrapper
     super(previousAuthorization);
   }
 
+  /**
+   * @param ctx
+   * @param ei
+   * @return
+   * @see de.micromata.genome.gwiki.model.GWikiAuthorization#isAllowToEdit(de.micromata.genome.gwiki.page.GWikiContext,
+   *      de.micromata.genome.gwiki.model.GWikiElementInfo)
+   */
+  public boolean isAllowToEdit(final GWikiContext ctx, final GWikiElementInfo ei)
+  {
+    if (isAllowTo(ctx, GWikiAuthorizationRights.GWIKI_ADMIN.name()) == true) {
+      return true;
+    }
+
+    if (super.isAllowTo(ctx, GWikiPlcRights.PLC_EDIT_ARTICLE.name()) && ei != null) {
+
+      boolean hasFileStatItem = ctx.runInTenantContext(ctx.getWikiWeb().getTenantId(), getWikiSelector(ctx),
+          new CallableX<Boolean, RuntimeException>() {
+            public Boolean call() throws RuntimeException
+            {
+              BranchFileStats stats = PlcUtils.getBranchFileStats(ctx);
+
+              if (stats != null) {
+                FileStatsDO statsForId = stats.getFileStatsForId(ei.getId());
+
+                if (statsForId == null) {
+                  return false;
+                } else {
+                  return true;
+                }
+              }
+              return false;
+            }
+          });
+
+      // Element has a branchFileStatEntry. Use the own authorization method.
+      if (hasFileStatItem) {
+        return ctx.runInTenantContext(ctx.getWikiWeb().getTenantId(), getWikiSelector(ctx), new CallableX<Boolean, RuntimeException>() {
+          public Boolean call() throws RuntimeException
+          {
+            BranchFileStats stats = PlcUtils.getBranchFileStats(ctx);
+            FileStatsDO statsForId = stats.getFileStatsForId(ei.getId());
+
+            if (StringUtils.equals(statsForId.getAssignedTo(), ctx.getWikiWeb().getAuthorization().getCurrentUserName(ctx))) {
+              return true;
+            }
+            return false;
+          }
+        });
+      }
+    }
+
+    return super.isAllowToEdit(ctx, ei);
+  }
+
+  private GWikiMultipleWikiSelector getWikiSelector(final GWikiContext ctx)
+  {
+    GWikiWikiSelector wikiSelector = ctx.getWikiWeb().getDaoContext().getWikiSelector();
+    if (wikiSelector == null) {
+      ctx.addValidationError("gwiki.error.tenantsNotSupported");
+      return null;
+    }
+
+    if (wikiSelector instanceof GWikiMultipleWikiSelector == true) {
+      GWikiMultipleWikiSelector multipleSelector = (GWikiMultipleWikiSelector) wikiSelector;
+      return multipleSelector;
+    }
+    return null;
+  }
 }
