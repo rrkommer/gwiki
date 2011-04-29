5e38825184a93055f2c65040e761ec29b5d98f17
Christian Claus <c.claus@micromata.de>
2011-04-29 07:03:39 +0000

    * moved path input field from edit page to a jquery dialog * enhanced gwiki css to display normal <a> tags, with corresponding class, exactly on the same way as submit-buttons

--- gwiki/src/main/resources/static/gwiki/gwikiedit-0.3.js
+++ gwiki/src/main/resources/static/gwiki/gwikiedit-0.3.js
@@ -361,3 +361,42 @@ function gwikiEditInsertTemplate(templateText)
 			});
 	};
 })(jQuery);
+
+function gwikiEditPagePath() {
+	
+	var path = window.parent.document.getElementById('editPagePath').value;
+	var modc = $("#editDialogBox");
+	var dlghtml = "<b>NOTE:</b> Use this function to manipulate the path at your own risk.<br /><br />" +
+			"<label for=\"pathtextfield\" style=\"margin-right:10px\">Path:</label><input size=\"44\" type=\"text\" id=\"pathtextfield\""
+				+ " value='" + path + "'/><br/>\n";
+
+	modc.html(dlghtml);
+	
+	var dialog = $("#editDialogBox").dialog({
+		width : 500,
+		modal : true,
+		open : function(event, ui) {
+			$("#pathtextfield").focus();
+		},
+		close : function(event, ui) {
+			$(dialog).dialog('destroy');
+		},
+		overlay : {
+			backgroundColor : '#000',
+			opacity : 0.5
+		},
+		buttons : {
+			'Cancel' : function() {
+				$(dialog).dialog('close');
+			},
+			'OK' : function() {
+				var value = $("#pathtextfield").attr('value');
+				$(dialog).dialog('close');
+				window.parent.document.getElementById('editPagePath').value = value;
+			}
+		},
+		Abbrechen : function() {
+			$(this).dialog('close');
+		}
+	});
+}
