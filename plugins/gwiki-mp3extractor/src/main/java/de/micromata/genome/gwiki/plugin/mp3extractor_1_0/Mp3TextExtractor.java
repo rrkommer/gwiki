9a887f16954e90e7d1bd54aebf75f5d6c6d125dd
Roger Kommer <r.kommer.extern@micromata.de>
2015-11-10 16:49:13 +0100

    Merged from sf svn

---
+++ plugins/gwiki-mp3extractor/src/main/java/de/micromata/genome/gwiki/plugin/mp3extractor_1_0/Mp3TextExtractor.java
@@ -0,0 +1,50 @@
+package de.micromata.genome.gwiki.plugin.mp3extractor_1_0;
+
+import java.io.InputStream;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.Mp3File;
+
+import de.micromata.genome.gwiki.model.GWikiLog;
+import de.micromata.genome.gwiki.page.attachments.TextExtractor;
+
+/**
+ * Simple mp3 extractor
+ * 
+ * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
+ * 
+ */
+public class Mp3TextExtractor implements TextExtractor
+{
+
+  private void appendIfExists(StringBuilder sb, String label, String data)
+  {
+    if (StringUtils.isBlank(data) == true) {
+      return;
+    }
+    sb.append(label).append(": ").append(data).append("\n");
+  }
+
+  @Override
+  public String extractText(String fileName, InputStream data)
+  {
+    StringBuilder sb = new StringBuilder();
+    try {
+      Mp3File mp3file = new Mp3File(fileName);
+      if (mp3file.hasId3v1Tag() == true) {
+        ID3v1 tag = mp3file.getId3v1Tag();
+        appendIfExists(sb, "Artist", tag.getArtist());
+        appendIfExists(sb, "Album", tag.getAlbum());
+        appendIfExists(sb, "Title", tag.getTitle());
+        appendIfExists(sb, "Track", tag.getTrack());
+        appendIfExists(sb, "Comment", tag.getComment());
+      }
+    } catch (Exception e) {
+      GWikiLog.note("Cannot not extract mp3 content: " + e.getMessage(), e);
+    }
+    return sb.toString();
+  }
+
+}
---
+++ plugins/gwiki-mp3extractor/src/main/java/de/micromata/genome/gwiki/plugin/mp3extractor_1_0/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
+ *
+ */
+package de.micromata.genome.gwiki.plugin.mp3extractor_1_0;
