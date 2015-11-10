20e861ba3212b2c274ccb6c8be2e850bc21068a0
Roger Kommer <r.kommer.extern@micromata.de>
2015-11-10 16:44:07 +0100

    Merged from sf svn

--- plugins/gwiki-feed/src/test/java/de/micromata/genome/gwiki/plugin/rssfeed_1_0/RSSFeedTest.java
+++ plugins/gwiki-feed/src/test/java/de/micromata/genome/gwiki/plugin/rssfeed_1_0/RSSFeedTest.java
@@ -2,7 +2,7 @@ package de.micromata.genome.gwiki.plugin.rssfeed_1_0;
 
 ////////////////////////////////////////////////////////////////////////////
 // 
-// Copyright (C) 2010 Micromata GmbH
+// Copyright (C) 2010-2013 Micromata GmbH / Roger Rene Kommer
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
