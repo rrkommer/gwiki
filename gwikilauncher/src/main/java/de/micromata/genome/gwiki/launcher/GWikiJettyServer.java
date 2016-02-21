9b8a54f8969ebc08ed0f14b74de2ff157d677621
Roger Kommer <r.kommer.extern@micromata.de>
2016-02-21 19:53:48 +0100

    GWIKI-271

---
+++ gwikilauncher/src/main/java/de/micromata/genome/gwiki/launcher/GWikiJettyServer.java
@@ -0,0 +1,97 @@
+package de.micromata.genome.gwiki.launcher;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import de.micromata.genome.gwiki.model.GWikiLog;
+import de.micromata.genome.gwiki.model.GWikiWeb;
+import de.micromata.genome.gwiki.model.config.GWikiBootstrapConfigLoader;
+import de.micromata.genome.gwiki.model.config.GWikiCpContextBootstrapConfigLoader;
+import de.micromata.genome.gwiki.model.config.GWikiDAOContext;
+import de.micromata.genome.gwiki.model.config.GwikiFileContextBootstrapConfigLoader;
+import de.micromata.genome.gwiki.page.GWikiContext;
+import de.micromata.genome.gwiki.page.GWikiStandaloneContext;
+import de.micromata.genome.gwiki.page.search.expr.SearchExpressionIndexerCallback;
+import de.micromata.genome.gwiki.web.GWikiServlet;
+import de.micromata.genome.util.runtime.CallableX;
+import de.micromata.genome.util.runtime.LocalSettings;
+import de.micromata.mgc.jettystarter.JettyConfigModel;
+import de.micromata.mgc.jettystarter.JettyServer;
+
+/**
+ * 
+ * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
+ *
+ */
+public class GWikiJettyServer extends JettyServer
+{
+  private GWikiDAOContext wikibootcfg;
+  private GWikiServlet wikiServlet;
+
+  public GWikiJettyServer(JettyConfigModel config)
+  {
+    super(config);
+  }
+
+  @Override
+  protected ServletContextHandler createContextHandler(JettyConfigModel config)
+  {
+
+    String contextFile = LocalSettings.get().get("gwiki.contextfile", "res:/StandaloneGWikiContext.xml");
+    GWikiBootstrapConfigLoader cfgLoader;
+    if (StringUtils.startsWith(contextFile, "res:") == true) {
+      String fileName = contextFile.substring("res:".length());
+      GWikiCpContextBootstrapConfigLoader cploader = new GWikiCpContextBootstrapConfigLoader();
+      cploader.setFileName(fileName);
+      cfgLoader = cploader;
+
+    } else {
+      GwikiFileContextBootstrapConfigLoader fcfgLoader = new GwikiFileContextBootstrapConfigLoader();
+      if (StringUtils.isNotBlank(contextFile) == true) {
+        fcfgLoader.setFileName(contextFile);
+      } else {
+        contextFile = ".";
+      }
+      cfgLoader = fcfgLoader;
+    }
+
+    wikibootcfg = cfgLoader.loadConfig(null);
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    wikiServlet = new GWikiServlet();
+    ServletHolder wikiServletHolder = new ServletHolder(wikiServlet);
+    wikiServlet.setDAOContext(wikibootcfg);
+    context.addServlet(wikiServletHolder, "/*");
+    return context;
+  }
+
+  public void buildIndex(GWikiServlet wikiServlet, String contextPath)
+  {
+    final GWikiWeb nwiki = new GWikiWeb(wikiServlet.getDAOContext());
+    String servletPath = "";
+    nwiki.setContextPath(contextPath);
+    nwiki.setServletPath(servletPath);
+    try {
+      final GWikiStandaloneContext ctx = new GWikiStandaloneContext(nwiki, wikiServlet, contextPath,
+          servletPath);
+      GWikiContext.setCurrent(ctx);
+      nwiki.loadWeb();
+      nwiki.runInPluginContext(new CallableX<Void, RuntimeException>()
+      {
+
+        @Override
+        public Void call() throws RuntimeException
+        {
+          SearchExpressionIndexerCallback scb = new SearchExpressionIndexerCallback();
+          scb.rebuildIndex(ctx, nwiki.getElementInfos(), true);
+          return null;
+        }
+      });
+    } catch (Exception ex) {
+      GWikiLog.error("Failed to build index: " + ex.getMessage(), ex);
+    } finally {
+      GWikiContext.setCurrent(null);
+    }
+
+  }
+}
---
+++ gwikilauncher/src/main/java/de/micromata/genome/gwiki/launcher/config/GWikiFilesystemConfigModel.java
@@ -0,0 +1,82 @@
+package de.micromata.genome.gwiki.launcher.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import de.micromata.genome.gwiki.spi.storage.LsFileSystemFactoryBean;
+import de.micromata.genome.util.runtime.config.ALocalSettingsPath;
+import de.micromata.genome.util.runtime.config.AbstractCompositLocalSettingsConfigModel;
+import de.micromata.genome.util.runtime.config.JdbcLocalSettingsConfigModel;
+import de.micromata.genome.util.validation.ValContext;
+
+/**
+ * 
+ * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
+ *
+ */
+public class GWikiFilesystemConfigModel extends AbstractCompositLocalSettingsConfigModel
+{
+  public static final String FILESYSTEM = LsFileSystemFactoryBean.LOCAL_FILE_SYSTEM;
+  public static final String DATABASE = LsFileSystemFactoryBean.JPA_FILE_SYSTEM;
+
+  @ALocalSettingsPath(key = "gwiki.storage.type", defaultValue = FILESYSTEM)
+  private String storageType;
+
+  @ALocalSettingsPath(key = "gwiki.storage.filesystem.path", defaultValue = "./gwiki")
+  private String storageFilePath;
+
+  private JdbcLocalSettingsConfigModel jdbcConfigModel = new JdbcLocalSettingsConfigModel("gwiki",
+      "jdbc for gwiki", "java:/comp/env/genome/jdbc/dsWeb");
+
+  @Override
+  public void validate(ValContext ctx)
+  {
+    // TODO RK
+  }
+
+  public JdbcLocalSettingsConfigModel getJdbcConfigModel()
+  {
+    return jdbcConfigModel;
+  }
+
+  public boolean isJdbcStorageType()
+  {
+    return "Database".equals(storageType);
+  }
+
+  public void setJdbcStorageType(boolean jdbc)
+  {
+    storageType = jdbc ? DATABASE : FILESYSTEM;
+  }
+
+  public List<String> getAvailableFileSystemTypes()
+  {
+    return Arrays.asList(FILESYSTEM, DATABASE);
+  }
+
+  public String getStorageType()
+  {
+    return storageType;
+  }
+
+  public void setStorageType(String storageType)
+  {
+    this.storageType = storageType;
+  }
+
+  public String getStorageFilePath()
+  {
+    return storageFilePath;
+  }
+
+  public void setStorageFilePath(String storageFilePath)
+  {
+    this.storageFilePath = storageFilePath;
+  }
+
+  public void setJdbcConfigModel(JdbcLocalSettingsConfigModel jdbcConfigModel)
+  {
+    this.jdbcConfigModel = jdbcConfigModel;
+  }
+
+}
