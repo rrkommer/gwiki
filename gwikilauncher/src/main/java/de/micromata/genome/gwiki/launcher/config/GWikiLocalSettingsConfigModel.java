13277d2b1b1e536cb31bdd5725bbbe26f50f58aa
Roger Kommer <r.kommer.extern@micromata.de>
2016-02-29 17:08:20 +0100

    GWIKI-272. Migrate to MGC: Use GLog instead of GWikiLog

--- gwikilauncher/src/main/java/de/micromata/genome/gwiki/launcher/config/GWikiLocalSettingsConfigModel.java
+++ gwikilauncher/src/main/java/de/micromata/genome/gwiki/launcher/config/GWikiLocalSettingsConfigModel.java
@@ -1,6 +1,9 @@
 package de.micromata.genome.gwiki.launcher.config;
 
+import org.apache.commons.codec.Charsets;
+
 import de.micromata.genome.util.runtime.config.AbstractCompositLocalSettingsConfigModel;
+import de.micromata.genome.util.runtime.config.AbstractTextConfigFileConfigModel;
 import de.micromata.genome.util.runtime.config.HibernateSchemaConfigModel;
 import de.micromata.genome.util.runtime.config.JdbcLocalSettingsConfigModel;
 import de.micromata.genome.util.runtime.config.LocalSettingsConfigModel;
@@ -16,16 +19,25 @@ import de.micromata.mgc.jettystarter.JettyConfigModel;
  */
 public class GWikiLocalSettingsConfigModel extends AbstractCompositLocalSettingsConfigModel
 {
+  @SuppressWarnings("unused")
   private LauncherLocalSettingsConfigModel launcherConfig = new LauncherLocalSettingsConfigModel();
 
+  @SuppressWarnings("unused")
   private JettyConfigModel jettyConfig = new JettyConfigModel();
 
+  @SuppressWarnings("unused")
   private GWikiSystemUserConfigModel systemUser = new GWikiSystemUserConfigModel();
 
+  @SuppressWarnings("unused")
   private GWikiFilesystemConfigModel fileSystem = new GWikiFilesystemConfigModel();
+
   private MailSessionLocalSettingsConfigModel emailConfig = new MailSessionLocalSettingsConfigModel("gwikimailsession");
+
   @SuppressWarnings("unused")
   private HibernateSchemaConfigModel hibernateSchemaConfig = new HibernateSchemaConfigModel();
+  @SuppressWarnings("unused")
+  private AbstractTextConfigFileConfigModel log4jConfig = new AbstractTextConfigFileConfigModel("Log4J",
+      "log4j.properties", Charsets.ISO_8859_1);
 
   public GWikiLocalSettingsConfigModel()
   {
