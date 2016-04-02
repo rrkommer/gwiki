44a1ab1c0901e828a6f3f6652cb993df09b32189
Roger Kommer <r.kommer.extern@micromata.de>
2016-04-02 22:21:59 +0200

    GWIKI-278. editor

--- gwiki/src/main/java/de/micromata/genome/gwiki/page/impl/wiki/macros/html/GWikiHtmlThMacro.java
+++ gwiki/src/main/java/de/micromata/genome/gwiki/page/impl/wiki/macros/html/GWikiHtmlThMacro.java
@@ -1,5 +1,6 @@
 package de.micromata.genome.gwiki.page.impl.wiki.macros.html;
 
+import de.micromata.genome.gwiki.page.impl.wiki.GWikiMacroRenderFlags;
 import de.micromata.genome.gwiki.page.impl.wiki.MacroInfo;
 import de.micromata.genome.gwiki.page.impl.wiki.macros.GWikiHtmlBodyRteTagMacro;
 
@@ -8,11 +9,11 @@ import de.micromata.genome.gwiki.page.impl.wiki.macros.GWikiHtmlBodyRteTagMacro;
  * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
  *
  */
-@MacroInfo(info = "Create an html th element")
+@MacroInfo(info = "Create an html th element",
+    renderFlags = { GWikiMacroRenderFlags.NoWrapWithP, GWikiMacroRenderFlags.TrimTextContent })
 public class GWikiHtmlThMacro extends GWikiHtmlBodyRteTagMacro
 {
   public GWikiHtmlThMacro()
   {
-    super(getStandardNestedBodyRenderFlags());
   }
 }
