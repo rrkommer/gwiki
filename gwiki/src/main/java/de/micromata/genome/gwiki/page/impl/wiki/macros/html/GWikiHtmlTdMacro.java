b9286a4aaf8d140c9ba098e3727e890985302e0f
Roger Kommer <r.kommer.extern@micromata.de>
2016-04-02 22:21:59 +0200

    GWIKI-278. editor

--- gwiki/src/main/java/de/micromata/genome/gwiki/page/impl/wiki/macros/html/GWikiHtmlTdMacro.java
+++ gwiki/src/main/java/de/micromata/genome/gwiki/page/impl/wiki/macros/html/GWikiHtmlTdMacro.java
@@ -1,5 +1,6 @@
 package de.micromata.genome.gwiki.page.impl.wiki.macros.html;
 
+import de.micromata.genome.gwiki.page.impl.wiki.GWikiMacroRenderFlags;
 import de.micromata.genome.gwiki.page.impl.wiki.MacroInfo;
 import de.micromata.genome.gwiki.page.impl.wiki.macros.GWikiHtmlBodyRteTagMacro;
 
@@ -8,11 +9,12 @@ import de.micromata.genome.gwiki.page.impl.wiki.macros.GWikiHtmlBodyRteTagMacro;
  * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
  *
  */
-@MacroInfo(info = "Create an html table &lt;td&gt;")
+@MacroInfo(info = "Create an html table &lt;td&gt;",
+    renderFlags = {
+        GWikiMacroRenderFlags.NoWrapWithP, GWikiMacroRenderFlags.TrimTextContent })
 public class GWikiHtmlTdMacro extends GWikiHtmlBodyRteTagMacro
 {
   public GWikiHtmlTdMacro()
   {
-    super(getStandardNestedBodyRenderFlags());
   }
 }
--- gwiki/src/main/java/de/micromata/genome/gwiki/page/impl/wiki/macros/html/GWikiHtmlTrMacro.java
+++ gwiki/src/main/java/de/micromata/genome/gwiki/page/impl/wiki/macros/html/GWikiHtmlTrMacro.java
@@ -1,5 +1,6 @@
 package de.micromata.genome.gwiki.page.impl.wiki.macros.html;
 
+import de.micromata.genome.gwiki.page.impl.wiki.GWikiMacroRenderFlags;
 import de.micromata.genome.gwiki.page.impl.wiki.MacroInfo;
 import de.micromata.genome.gwiki.page.impl.wiki.macros.GWikiHtmlBodyRteTagMacro;
 
@@ -8,11 +9,13 @@ import de.micromata.genome.gwiki.page.impl.wiki.macros.GWikiHtmlBodyRteTagMacro;
  * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
  *
  */
-@MacroInfo(info = "Create an html table tr")
+@MacroInfo(info = "Create an html table tr",
+    renderFlags = {
+        GWikiMacroRenderFlags.NoWrapWithP, GWikiMacroRenderFlags.TrimTextContent,
+        GWikiMacroRenderFlags.NewLineBeforeEnd })
 public class GWikiHtmlTrMacro extends GWikiHtmlBodyRteTagMacro
 {
   public GWikiHtmlTrMacro()
   {
-    super(getStandardNestedBodyRenderFlags());
   }
 }
